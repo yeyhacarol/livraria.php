d9e70596d5f60e3dd31fb7e7edcda1bf07bf63ce
CAROLINA VITÓRIA CHAVES SILVA <21276093@CFP127ALUNO.LOCAL>
2022-03-31 17:00:51 -0300

    iniciando programação com banco de dados; crud

---
+++ cms/controller/controllerContatos.php
@@ -0,0 +1,16 @@
+<?php 
+/* arquivo responsável pela manipulação dos dados de contato. ponte entre view e model; autora: Carolina Silva; data de criação: 31/04/2022; versão: 1.0*/
+
+/* lembrando que essas funções recebem os dados da view (front-end) e encaminham para a model (banco de dados) */
+
+//função para inserir os contatos da view na model {
+function inserirContato() {
+
+}
+
+//função para listar contatos na tela de cms
+function listarContatos() {
+
+}
+
+?>
---
+++ cms/router.php
@@ -0,0 +1,5 @@
+<?php 
+/* arquivo de rota para receber requisições da view  e enviar para controler pois a view não pode manipular diretamente o banco; 
+autora: Carolina Silva; data de criação: 31/03/2022; versão: 1.0 */
+
+?>
